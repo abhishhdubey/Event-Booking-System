d84eb573c034edbc9694bd2fc223026f431b789b
Abhishek Dubey <abhishekdubey.dx24@acropolis.in>
2026-03-28 03:19:35 +0530

    Initial commit

--- test_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'config/config.php';
-$movies = $conn->query("SHOW COLUMNS FROM movies");
-echo "MOVIES COLUMNS:\n";
-while($r = $movies->fetch_assoc()) echo $r['Field'] . " - " . $r['Type'] . "\n";
-echo "\nTHEATRES COLUMNS:\n";
-$theatres = $conn->query("SHOW COLUMNS FROM theatres");
-while($r = $theatres->fetch_assoc()) echo $r['Field'] . " - " . $r['Type'] . "\n";
-?>
